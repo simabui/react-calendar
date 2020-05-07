f1beea72646c73448275ac8833ce41b407aa2af9
simabui <sima_sima@live.ru>
2020-05-07 22:53:35 +0300

    Updates

---
+++ precache-manifest.07e5617141bcf14547a6f9d6862f8c2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "83cfcb6db61edd6c5441",
+    "url": "/static/css/main.e97f46d6.chunk.css"
+  },
+  {
+    "revision": "83cfcb6db61edd6c5441",
+    "url": "/static/js/main.641402a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c0ea35107891aa96d63a",
+    "url": "/static/css/2.81f9e09e.chunk.css"
+  },
+  {
+    "revision": "c0ea35107891aa96d63a",
+    "url": "/static/js/2.8a306e34.chunk.js"
+  },
+  {
+    "revision": "788157d25c042a65e25bc2f1812a8d09",
+    "url": "/index.html"
+  }
+];
